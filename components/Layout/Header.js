5dea570ba0068a194e10256d4335254bd956e857
suraartut <suraartut@gmail.com>
2023-08-14 23:43:18 +0300

    update: Header

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="bg-transparent hover:bg-red-500 h-20 w-full">
+      <div className="px-10 flex h-full items-center justify-between text-center">
+        <div>Logo</div>
+        <div>
+          <ul className="w-full flex gap-5">
+            <li>Home</li>
+            <li>Software</li>
+            <li>Industry</li>
+            <li>Support</li>
+            <li>Company</li>
+            <li>News</li>
+            <li>Download</li>
+          </ul>
+        </div>
+        <div>
+          <ul className="w-full flex gap-5">
+            <li>My Account</li>
+            <li>Language</li>
+            <li>Search</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
