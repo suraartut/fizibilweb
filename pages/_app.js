7a505e7ccccb540722a8d0ff5b98bd16728cf56c
Merve Şura ARTUT <suraartut@gmail.com>
2023-08-14 17:24:51 +0300

    First Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import Footer from "@/components/Layout/Footer";
+import Header from "@/components/Layout/Header";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
