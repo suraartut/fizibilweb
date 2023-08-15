6ad8773ed31d787ee7750878271d2334df752f64
Merve Şura ARTUT <suraartut@gmail.com>
2023-08-15 18:31:55 +0300

    update: toastify

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import Footer from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
 import "@/styles/globals.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <ToastContainer rtl={Lang == "ar" ? true : false} />
       <Footer />
     </>
   );
