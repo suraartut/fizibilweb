01fa77b6efc21d4b7691e1b62c0c2c06db67b139
suraartut <suraartut@gmail.com>
2023-09-22 23:51:10 +0300

    update: deneme

--- components/Home/BannerTopMobile/BannerMobile.js
+++ components/Home/BannerTopMobile/BannerMobile.js
@@ -26,21 +26,26 @@ const BannerMobile = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <img
+          <Image
             src="/assets/Images/mobil-slider/bilgisayarli-goru.png"
+            width={390}
+            height={360}
             alt="Bilgisayarlı Görü"
-            className="object-fit object-cover"
+            className="contrast-50"
           />
-          <div className="absolute bg-black bg-opacity-40 p-6">
-            <h3 className="font-bold text-white">Bilgisayarlı Görü Sistemi</h3>
-            <p className="my-3 text-base">Açıklama</p>
-            <div className="h-1 bg-[#2e8fce] mt-4 mb-8"></div>
-            <Link
-              href="/sayac"
-              className="text-white text-base px-12 py-3 border-2 border-white"
-            >
-              Keşfet
-            </Link>
+          <div className="w-full absolute">
+            <div className="p-10 text-end">
+              <h3 className="font-bold text-white">
+                Bilgisayarlı Görü Sistemi
+              </h3>
+              <p className="pt-3 pb-8 text-base text-white">Açıklama</p>
+              <Link
+                href="/"
+                className="text-white text-sm px-12 py-3 border-2 border-[#2e8fce] bg-[#2e8fce] uppercase font-semibold hover:text-[#2e8fce] hover:border-white hover:bg-white tracking-wider w-full items-center"
+              >
+                Keşfedİn
+              </Link>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -49,6 +54,7 @@ const BannerMobile = () => {
             width={390}
             height={360}
             alt="Doğalgaz Sayaç Okuma"
+            className="contrast-50"
           />
         </SwiperSlide>
         <SwiperSlide>
@@ -57,6 +63,7 @@ const BannerMobile = () => {
             width={390}
             height={360}
             alt="Elektrik Sayacı Okuma"
+            className="contrast-50"
           />
         </SwiperSlide>
       </Swiper>
