64ccf0f6d7a65df3d9791486730238e553f750aa
suraartut <suraartut@gmail.com>
2024-01-01 22:05:30 +0300

    update: favicon and title

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Header from "@/components/Layout/Header";
 import MobileHeader from "@/components/Layout/MobileHeader";
 import "@/styles/globals.css";
 import Router, { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   Router.events.on("routeChangeComplete", (url, { shallow }) => {
@@ -12,6 +13,15 @@ export default function App({ Component, pageProps }) {
   });
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/fizibil-favicon.ico" />
+        <title>Fizibil Mühendislik & Yazılım</title>
+        <meta
+          property="og:title"
+          content="Fizibil Mühendislik & Yazılım"
+          key="title"
+        />
+      </Head>
       <Header />
       <MobileHeader />
       <Component {...pageProps} />
