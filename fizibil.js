cb298637e1bb70819949e6edd506c552cf495c4e
suraartut <suraartut@gmail.com>
2024-01-02 16:33:16 +0300

    update:

---
+++ fizibil.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const next = require("next");
+const { join } = require("path");
+const url = require("url");
+
+const dev = false;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = 3042;
+var httpsRedirect = require("express-https-redirect");
+
+app.prepare().then(() => {
+  const server = express();
+
+  //server.use('/', httpsRedirect(true));
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:3043");
+  });
+});
